38af0aee8523375fcb483aa46ec8b7f9682259d6
JadeDoyle <pierre.boniface90@gmail.com>
2022-09-21 15:17:17 +0200

    Update util.py

--- util.py
+++ util.py
@@ -5,3 +5,20 @@ Created on Wed Sep 21 14:48:05 2022
 @author: Pierre Boniface
 """
 
+def initialize(L):
+
+    rul = []
+    res = [0] * 2**L
+    n=0
+
+    for _ in range(2**L):
+        rul.append(format(n, ('0' + str(L) + 'b')))
+        n = n+1
+
+    dic = dict(zip(rul,res))
+
+    print(dic)
+
+
+
+initialize(6)
