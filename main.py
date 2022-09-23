4b39ef2208a0dcd8272259807e52dffe92ba5071
JadeDoyle <pierre.boniface90@gmail.com>
2022-09-23 14:58:52 +0200

    git can be enoying

---
+++ main.py
@@ -0,0 +1,22 @@
+import gym
+import util
+
+#initate
+env = gym.make("CartPole-v1", render_mode="human")  #render_mode can either be none (headless) or human (graphical)
+observation, info = env.reset() #(seed=42) If sample() is to be used to randomize the actionspace, env.reset needs to be seeded for repeatability
+
+#initiate CA world
+worldWidth = 8
+windowLength = 3
+votingMethod = 'equal_split'
+genome = 34 #must be less or equal to 2**2**windowlength (for windowlength of 3, genome = (0,256))
+
+for _ in range(500):
+    action = util.get_action(worldWidth, observation[2], windowLength, votingMethod, genome)
+    print(action)
+    #env.action_space.sample() can be used to randomize the action
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
