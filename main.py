9a7ac5a014dad0c53d18a9f0f27fc4f2910f8222
JadeDoyle <pierre.boniface90@gmail.com>
2022-09-24 19:56:50 +0200

    reality check

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ env = gym.make("CartPole-v1")  #render_mode = 'human' (graphical)
 observation, info = env.reset() #(seed=42) If sample() is to be used to randomize the actionspace, env.reset needs to be seeded for repeatability
 
 #initiate CA world
-worldWidth = 8 #must be even number
-windowLength = 3 #must be odd number
+worldWidth = 16 #must be even number
+windowLength = 5 #must be odd (3 gives a genome length of 8 bit, 5; 32, 7, 125, 9, 256, 12, 1024)
 votingMethod = 'equal_split'
-maxSteps = 100 #this allows the genom to respawn, if the simulation is terminated
+maxSteps = 200 #this allows the genom to respawn, if the simulation is terminated
 batchSize = 100
 
 parentGenomes = []
@@ -22,28 +22,28 @@ for _ in range(batchSize):
     genomeEpisodes = 0 #number of episodes within the maxSteps
     genomeReward = 0 #accumulative reward over maxSteps
 
-    genome = random.randint(0,255) #must be less or equal to 2**2**windowlength (for windowlength of 3, genome = (0,255))
-    parentGenomes.append(format(genome, ('0' + str(2 ** windowLength) + 'b')))
+    genome = random.randint(0, 2**(2**windowLength)-1) #must be less or equal to 2**(2**windowLength)-1 (for windowlength of 3, genome = (0,255))
+    parentGenomes.append(format(genome, ('0' + str(2**windowLength) + 'b')))
 
     for _ in range(maxSteps):
         action = util.get_action(worldWidth, observation[2], windowLength, votingMethod, genome)
         #env.action_space.sample() can be used to randomize the action
         observation, reward, terminated, truncated, info = env.step(action)
-        genomeReward +=1
+        genomeReward += 1
 
         if terminated or truncated:
             observation, info = env.reset()
-            genomeEpisodes +=1
+            genomeEpisodes += 1
 
     parentResults.append(round(genomeReward/genomeEpisodes, 2))
 
-env.close()
-
 parents = {k: v for k, v in sorted(dict(zip(parentGenomes, parentResults)).items(), key=lambda item: item[1])}
+
+env.close()
 
 print(parents, '\n')
 
-print('offspring', util.evolve(parents, 0.2, 'one-point-crossover', 'deterministically', 0.8))
+print('offspring \n', util.evolve(parents, 0.2, 'one-point-crossover', 'deterministically', 0.8))
 
 
 #print (f"Episodes: {genomeEpisodes}")
