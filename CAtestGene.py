f1dee74fc46098cd03c156f94ac3ee93ee12c739
JadeDoyle <pierre.boniface90@gmail.com>
2022-11-04 11:56:16 +0100

    comments

--- CAtestGene.py
+++ CAtestGene.py
@@ -1,31 +1,42 @@
 import gym
 import src.CAutil as CAutil
 
+# predefine genome for dtesting
 genome = '11100110011011000010101101100001110011010111001010001000000101111011101110001000010010010100011111100101110001001101001111011010' #15,7,5,6
 
+# initialize simulation
 config = CAutil.get_config()
 env = gym.make("CartPole-v1", render_mode='human')
 observation, info = env.reset()
 
 rules = dict(zip(CAutil.set_condition_list(), CAutil.initialize_rules(genome)))
 
+# give the genome n chances
 for _ in range(config['maxAttempts']):
+    
+    # counts timesteps
+    steps = 0
 
-    totReward = 0
-
+    # maximum number of time steps befor termination
     for _ in range(5000):
-
+        
+        # get action based on environment state
         observation, reward, terminated, truncated, info = env.step(CAutil.get_action(observation, rules))
 
-        totReward += 1
+        # increment step counter
+        steps += 1
 
+        # terminate environment if genome fails
         if terminated:
-
+            
+            # reset environment
             observation, info = env.reset()
-
+            
+            # exit current run
             break
+    
+    # print time step reached
+    print(steps)
 
-    print(totReward)
-
-
+# close environment
 env.close()
--- NNtestGene.py
+++ NNtestGene.py
@@ -1,29 +1,40 @@
 import gym
 import src.NNutil as NNutil
 
+# predefine genome for dtesting
 genome = [0.83151852849641, 0.5667395410331488, 1.0377943781978949, 0.9111872106718677]
 
+# initialize simulation
 config = NNutil.get_config()
 env = gym.make("CartPole-v1", render_mode='human')
 observation, info = env.reset()
 
+# give the genome n chances
 for _ in range(config['maxAttempts']):
+    
+    # counts timesteps
+    steps = 0
 
-    totReward = 0
-
+    # maximum number of time steps befor termination
     for _ in range(5000):
-
+        
+        # get action based on environment state
         observation, reward, terminated, truncated, info = env.step(NNutil.get_action(observation, genome))
 
-        totReward += 1
+        # increment step counter
+        steps += 1
 
+        # terminate environment if genome fails
         if terminated:
-
+            
+            # reset environment
             observation, info = env.reset()
-
+            
+            # exit current run
             break
+    
+    # print time step reached
+    print(steps)
 
-    print(totReward)
-
-
+# close environment
 env.close()
